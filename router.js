c34db89249ae3d47da73aa30bba453c16cc10d62
System <none>
2017-03-08 15:46:42 +0000

    fixed

--- router.js
+++ router.js
@@ -1,8 +1,34 @@
+const moment = require('moment');
+
 module.exports = function(app){
     app.get("/", function (request, response) {
       response.sendFile(__dirname + '/views/index.html');
     });
 
-    
+    app.get('/:resource', function(req, res){
+      var data = req.params.resource;
+      var unix;
+      var natural;
+      if(moment(data).isValid()){
+        console.log("valido data");
+         unix = moment(data).format('X');
+         natural = moment(data).format("LL");
+       }
+      else if(moment.unix(data).isValid()){
+        console.log("valido unix");
+          unix = data;
+          natural = moment.unix(data).format("LL");
+      }
+      else{
+        console.log("non valido");
+        unix = natural = null;
+      }
+      /*if(unix === 'Invalid date')
+        unix = null;
+      if(natural === 'Invalid date')
+        natural = null;*/
+    res.send({ unix,natural });
+  
+})
 
 }
--- server.js
+++ server.js
@@ -4,32 +4,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
 const router = require('./router');
-const moment = require('moment');
+
 
 app.use(express.static('public'));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
 
-app.get('/:resource', function(req, res){
-    var data = req.params.resource;
-    var unix;
-    var natural;
-    
-    if(moment(data).isValid() || moment(data, 'Unix').isValid()){
-       unix = moment(data).format('X');
-       natural = moment(data).format("LL");
-     }
-    else{
-      unix = natural = null;
-    }
-  res.send({ unix,natural });
-  
-    
-})
 
 
 
